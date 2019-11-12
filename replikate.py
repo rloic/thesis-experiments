1d9aebcb4067fbc04cdf12f59c267abc89da138d
rloic <loic.rouquette@insa-lyon.fr>
2019-11-12 08:21:16 +0100

    Fix replikate script

--- replikate.py
+++ replikate.py
@@ -161,32 +161,33 @@ class EMailer:
     def send_mail(self, receivers: List[str], subject: str, message: str, files: List[Tuple[str, str]] = []):
         if self.server_parameters.authentication == 'STARTTLS':
             context = ssl.create_default_context()
-            with smtplib.SMTP(self.server_parameters.host, self.server_parameters.port) as smtp:
-                smtp.starttls(context=context)
-                smtp.login(
-                    self.mail_account.user_name,
-                    self.pwd
-                )
-                mail = MIMEMultipart()
-                mail['From'] = self.mail_account.mail
-                mail['To'] = ', '.join(receivers)
-                mail['Date'] = formatdate(localtime=True)
-                mail['Subject'] = subject
-                mail.attach(MIMEText(message, 'html'))
-                for (f, name) in files:
-                    part = MIMEBase('application', 'octet-stream')
-                    part.set_payload(open(f, 'rb').read())
-                    encoders.encode_base64(part)
-                    part.add_header('Content-Disposition', 'piece; filename= %s' % name)
-                    mail.attach(part)
-                try:
-                    smtp.sendmail(
-                        self.mail_account.mail,
-                        receivers,
-                        mail.as_string()
+            try:
+                with smtplib.SMTP(self.server_parameters.host, self.server_parameters.port) as smtp:
+                    smtp.starttls(context=context)
+                    smtp.login(
+                        self.mail_account.user_name,
+                        self.pwd
                     )
-                except Exception as e:
-                    print(e)
+                    mail = MIMEMultipart()
+                    mail['From'] = self.mail_account.mail
+                    mail['To'] = ', '.join(receivers)
+                    mail['Date'] = formatdate(localtime=True)
+                    mail['Subject'] = subject
+                    mail.attach(MIMEText(message, 'html'))
+                    for (f, name) in files:
+                        part = MIMEBase('application', 'octet-stream')
+                        part.set_payload(open(f, 'rb').read())
+                        encoders.encode_base64(part)
+                        part.add_header('Content-Disposition', 'piece; filename= %s' % name)
+                        mail.attach(part)
+
+                        smtp.sendmail(
+                            self.mail_account.mail,
+                            receivers,
+                            mail.as_string()
+                        )
+            except smtplib.SMTPException:
+                print('Cannot send email')
 
 
 def versioning_from_yml(yml) -> Versioning:
